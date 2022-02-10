6e691a03bbeed8d55153e0c00f6288e1fd95624d
Al Zee <z@alz.ee>
2022-02-10 19:41:56 +0800

    chart.js in chart0 instead of bootstrap progress bar

---
+++ js/initChart0.js
@@ -0,0 +1,35 @@
+const labels0 = ['热轧','冷轧','钢管', '硅钢', '型钢', '中厚板', '不锈钢', '特钢'];
+const data0 = {
+  labels: labels0,
+  datasets: [
+    {
+      label: '金额',
+      data: [4800,8300,7200,6200,7100,9800,8100,8800],
+      borderColor: '#36a2eb',
+      backgroundColor: '#36a2eb',
+    }
+  ]
+};
+
+const config0 = {
+  type: 'bar',
+  data: data0,
+  options: {
+    indexAxis: 'y',
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+      legend: {
+        display: false,
+        position: 'top',
+      },
+      title: {
+        display: false,
+        text: '当季交易商品排行'
+      }
+    }
+  },
+};
+
+const chart0 = new Chart(document.getElementById('chart0'), config0);
+chart0.height = 500;
