9bbd63826d3e34a772f368d697e68afbb2f181c2
Al Zee <z@alz.ee>
2022-03-24 12:18:43 +0800

    nth

--- js/initChart2.js
+++ js/initChart2.js
@@ -1,74 +1,61 @@
-const labels2 = ['一月','二月','三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'];
-const data2 = {
-  labels: labels2,
-  datasets: [
-    {
-      label: '茅箭区',
-      data: [6200,2300,400,4500,2700,1800,3900,2200,2500,1900,3400,4000],
-      borderColor: '#05309c',
-      backgroundColor: '#05309c',
-      fill: true
-    },
-    {
-      label: '张湾区',
-      data: [4800,8300,2200,5200,7100,1100,9100,6000,5000,7000,4000,8000],
-      borderColor: '#36a2eb',
-      backgroundColor: '#36a2eb',
-      fill: true
-    },
-    {
-      label: '白浪开发区',
-      data: [9200,2300,7400,3500,2700,5800,2900,3000,7000,4000,6000,7800],
-      borderColor: '#4bc0c0',
-      backgroundColor: '#4bc0c0',
-      fill: true
-    },
-    //{
-    //  label: 'My Fourth dataset',
-    //  data: [62,33,24,75,57,38,90],
-    //  borderColor: '#859adc',
-    //  backgroundColor: '#859adc',
-    //  fill: true
-    //}
-  ]
-};
+fetch('https://mysteelapi.steelphone.com/tpl/zhanting_data.html?indexCodes=SS_0000156821&startTime=2021-01-01')
+    .then(response => response.text())
+    .then(data => {
+        data = data.replace('callback(', '');
+        data = data.replace(');', '');
+        data = JSON.parse(data);
+        let labels2 = data.xAxis;
+        // let data2 = data.datas[0].yAxis;
+        let data2 =  {
+            labels: data.xAxis,
+            datasets: [
+                {
+                    label: 'test',
+                    data: data.datas[0].yAxis,
+                    borderColor: '#05309c',
+                    backgroundColor: '#05309c',
+                    fill: true
+                }
+            ]
+        };
 
-const config2 = {
-  type: 'line',
-  data: data2,
-  options: {
-    responsive: true,
-    plugins: {
-      title: {
-        display: false,
-        text: (ctx) => '区域交易分析'
-      },
-      tooltip: {
-        mode: 'index'
-      },
-    },
-    interaction: {
-      mode: 'nearest',
-      axis: 'x',
-      intersect: false
-    },
-    scales: {
-      x: {
-        title: {
-          display: true,
-          text: ''
-        }
-      },
-      y: {
-        stacked: true,
-        title: {
-          display: true,
-          text: ''
-        }
-      }
-    }
-  }
-};
-
-const chart2 = new Chart(document.getElementById('chart2'), config2);
-chart2.height = 300;
+        const config2 = {
+            type: 'line',
+            data: data2,
+            options: {
+                responsive: true,
+                plugins: {
+                    title: {
+                        display: false,
+                        text: (ctx) => '区域交易分析'
+                    },
+                    tooltip: {
+                        mode: 'index'
+                    },
+                },
+                interaction: {
+                    mode: 'nearest',
+                    axis: 'x',
+                    intersect: false
+                },
+                scales: {
+                    x: {
+                        title: {
+                            display: true,
+                            text: ''
+                        }
+                    },
+                    y: {
+                        stacked: true,
+                        title: {
+                            display: true,
+                            text: ''
+                        }
+                    }
+                }
+            }
+        };
+        // console.log(data);
+        const chart2 = new Chart(document.getElementById('chart2'), config2);
+        //chart2.height = 300;
+    });
